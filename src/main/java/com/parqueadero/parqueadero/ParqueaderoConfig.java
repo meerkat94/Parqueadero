2bd6528787314c53855c3f690b5ac6ea72cf02b1
Sebastian Eusse <s-e-b-a-s.94@hotmail.com>
2017-10-15 10:11:45 -0500

    cors filter

--- src/main/java/com/parqueadero/parqueadero/ParqueaderoConfig.java
+++ src/main/java/com/parqueadero/parqueadero/ParqueaderoConfig.java
@@ -2,8 +2,13 @@ package com.parqueadero.parqueadero;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import dominio.Parqueadero;
 import dominio.Vigilante;
 import dominio.reglas.ReglaCapacidadDelParqueadero;
@@ -41,7 +46,21 @@ public class ParqueaderoConfig {
 	public List<ReglasEgresoParqueadero> crearReglasEgreso(){
 	return new ArrayList<>();
 	}
-	
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter();
+    }
 
 
 }
